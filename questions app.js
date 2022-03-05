4afa608513b3797e823ffb557a90bef785367d83
siu29ss <99139280+siu29ss@users.noreply.github.com>
2022-03-05 23:56:28 +0530

    Add files via upload

---
+++ questions app.js
@@ -0,0 +1,129 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is lead vocalist of the band Coldplay?",
+    answer: "Chris Martin",
+    options: [
+    "Chris Martin",
+        "Chris Brown",
+        "Chris Connelly ",
+        "Chris Wallace"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which singer was featured in Jack&U (Skrillex & Diplo) 2015 song Where R U Now?",
+    answer: "Justin Bieber",
+    options: ["Selena Gomez",
+    "Ellie Goulding",
+    "Justin Bieber",
+    "The Weeknd"
+      
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which was Zayn Malik's debut album as a solo artist?",
+  
+    answer: "Mind Of Mine",
+    options: [
+      
+        "Icarus Falls",
+        "Mind Of Mine",
+        "Nobody Is Listening",
+        "Up All Night"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the singer of the hit track Little Red Corvette?",
+    
+    answer: "Prince",
+    options: [
+     
+        "Usher",
+        "Michael Jackson",
+        "Prince",
+        "Marvin Gaye"
+    ]
+  },
+    {
+    numb: 5,
+    question: "With which rapper did Ariana Grande collaberate in her song The Way?",
+    
+    answer: "Mac Miller",
+    options: [
+      
+        "Megan Thee Stallion",
+        "Machine Gun Kelly",
+        "Eminem",
+        "Mac Miller"
+    ]
+  },
+  
+
+     {
+     numb: 6,
+     question: "Which song is Zayn Malik's highest charted single as a solo artist till date?",
+     
+     answer: "Pillowtalk",
+     options: [
+      "I Don't Wanna Live Forever",
+      "Pillowtalk",
+      "Let Me",
+      "Dusk Till Dawn"
+     ]
+   },
+     {
+     numb: 7,
+     question: "Which Taylor Swift album is Blank Space on?",
+     
+     answer: "1989",
+     options: [
+      
+        "Taylor Swift",
+        "Speak Now",
+        "Red",
+        "1989"
+      ]
+    },
+    {
+      numb: 8,
+      question: "Which artist is the lead singer and guitarist for the indie rock band The 1975?",
+      
+      answer: "Matthew Healy",
+      options: [
+        "Matthew Healy",
+        "Marvin Gaye",
+        "Marren Morris",
+        "Harry Styles"
+      ]
+    },
+      {
+      numb: 9,
+      question: "In which video does Michael Jackson plays a zombie?",
+      
+      answer: "Thriller",
+      options: [
+        
+        "Thriller",
+        "Bad",
+        "Beat It",
+        "Hollywood Tonight"
+       ]
+     },
+     {
+      numb: 10,
+      question: "In which Jonas Brothers video did actress Camilla Belle was featured?",
+      
+      answer: "Love Bug",
+      options: [
+        
+        "Love Bug",
+        "Paranoid",
+        "Burnin' Up",
+        "Jealous"
+       ]
+     },
+];
